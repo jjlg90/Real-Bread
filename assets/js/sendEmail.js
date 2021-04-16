134dca438c69be1d393bf43d2e55345f2085fc34
jjlg90 <jjlimag2@gmail.com>
2021-04-16 14:05:17 +0000

    Adding sendEmail.js script.Tidying up index.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,11 @@
+function sendEmail(contactForm) {
+    emailjs.send("service_sspxmpc", "realBread", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_message": contactForm.message.value
+    }).then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+    }, function (error) {
+        console.log('FAILED...', error);
+    })
+}
