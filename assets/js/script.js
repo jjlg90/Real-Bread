19c23595a6ba4dc31ee42dee977f411f583a9bc5
jjlg90 <jjlimag2@gmail.com>
2021-04-19 15:51:25 +0000

    Removing unused script (header shade) from script.js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,6 @@ $(".js-scroll-trigger").click(function () {
     $(".navbar-collapse").collapse("hide");
 });
 
-/* This script changes the background color of the header when the user scrolls down through the site*/
-window.onscroll = function () {
-    const top = window.scrollY;
-    if (top >= 100) {
-        header.classList.add("active")
-    } else {
-        header.classList.remove("active");
-    }
-
-}
-
 /* This script shift between tabs in Our Products section*/
 function openProductTab(evt, productName) {
     // Declare all variables
