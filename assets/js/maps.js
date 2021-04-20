a52327fd6040d78d5bed5d7d0d29d53e660e4cfb
jjlg90 <jjlimag2@gmail.com>
2021-04-20 17:19:37 +0000

    Modifying code in maps.js to incorporate infoWindows (incomplete)

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -14,25 +14,35 @@ function initMap() {
 
     var locations = [{
 
-            //tag A
+            coords:{
             lat: 52.37643384193294,
             lng: 4.631371396030426
+            },
+            info:"Haarlem - Grote Houtstraat 163"
         },
         {
-            //tag B
+            coords:{
             lat: 52.37369052720405,
             lng: 4.877853119049435
+            },
+            info:"Amsterdam - Rozengracht 180"
         },
         {
-            //tag C
+            coords:{
             lat: 52.075448555229514,
             lng: 4.3166986533105876
+            },
+            info:`<h2>The Hague - Wagenstraat 182</h2>`
         }
     ];
 
+    var infoWindow = new google.maps.InfoWindow({
+    content: location.info,
+  });
+
     var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
-            position: location,
+            position: location.coords,
             label: labels[i % labels.length]
         });
     });
@@ -40,4 +50,9 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers, {
         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
+
+    marker.addListener('click', function () {
+            infoWindow.open(map, marker);
+        });
+
 }
