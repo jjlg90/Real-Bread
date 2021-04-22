e46581cb66e4f31a27ebedb6e5bf33589d24a4db
jjlg90 <jjlimag2@gmail.com>
2021-04-22 11:54:46 +0000

    Finishing google maps API integration in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,61 +1,52 @@
 /*This script renders the google maps API in Our Locations section*/
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        //initial focused area
+function initMap(){
+    var options ={
+        // Map options
         zoom: 9,
         center: {
             lat: 52.30517312711107,
             lng: 4.675973153140284
         }
-    });
-
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var locations = [{
-
-        //location A
-        coords: {
-            lat: 52.37643384193294,
-            lng: 4.631371396030426
-        },
-        info: "Haarlem - Grote Houtstraat 163"
-    },
-    {
-        //Location B
-        coords: {
-            lat: 52.37369052720405,
-            lng: 4.877853119049435
-        },
-        info: "Amsterdam - Rozengracht 180"
-    },
-    {
-        //Location C
-        coords: {
-            lat: 52.075448555229514,
-            lng: 4.3166986533105876
-        },
-        info: `<h2>The Hague - Wagenstraat 182</h2>`
     }
-    ];
 
-    var infoWindow = new google.maps.InfoWindow({
-        content: location.info,
+// New map
+var map = new google.maps.Map(document.getElementById('map'), options);
+
+// Add marker call-out
+   
+addMarker({
+    coords: {lat:52.37643384193294,lng:4.631371396030426},
+    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Haarlem</h2><h4>Grote Houtstraat 163</h4></div>` 
+});
+
+addMarker({
+    coords: {lat: 52.37369052720405,
+            lng: 4.877853119049435},
+    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Amsterdam</h2><h4>Rozengracht 180</h4></div>` 
+});
+
+addMarker({
+    coords: {lat: 52.075448555229514,
+            lng: 4.3166986533105876},
+    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>The Hague</h2><h4>Wagenstraat 182</h4></div>` 
+});
+
+// Add Marker Function
+    function addMarker(props){
+        var marker = new google.maps.Marker({
+        position: props.coords,
+        map:map,
     });
+    
+    if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+        content:props.content});
 
-    var markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location.coords,
-            label: labels[i % labels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
-
-    marker.addListener('click', function () {
+    marker.addListener('click', function(){
         infoWindow.open(map, marker);
-    });
+    })
 
+    }
+    }
 }
