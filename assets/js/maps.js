3ed56aa6bbc60087affa9b736808925ee6795aaf
jjlg90 <jjlimag2@gmail.com>
2021-04-22 20:32:27 +0000

    Updating and styling Our Locations section

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,6 @@
-/*This script renders the google maps API in Our Locations section*/
-
-function initMap(){
-    var options ={
+//This function renders the google maps API and markers in Our Locations section
+function initMap() {
+    var options = {
         // Map options
         zoom: 9,
         center: {
@@ -10,43 +9,57 @@ function initMap(){
         }
     }
 
-// New map
-var map = new google.maps.Map(document.getElementById('map'), options);
+    // New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
-// Add marker call-out
-   
-addMarker({
-    coords: {lat:52.37643384193294,lng:4.631371396030426},
-    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Haarlem</h2><h4>Grote Houtstraat 163</h4></div>` 
-});
+    // Add marker call-out
 
-addMarker({
-    coords: {lat: 52.37369052720405,
-            lng: 4.877853119049435},
-    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Amsterdam</h2><h4>Rozengracht 180</h4></div>` 
-});
-
-addMarker({
-    coords: {lat: 52.075448555229514,
-            lng: 4.3166986533105876},
-    content:`<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>The Hague</h2><h4>Wagenstraat 182</h4></div>` 
-});
-
-// Add Marker Function
-    function addMarker(props){
-        var marker = new google.maps.Marker({
-        position: props.coords,
-        map:map,
+    addMarker({
+        coords: { lat: 52.37643384193294, lng: 4.631371396030426 },
+        content: `<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Haarlem</h2><h4>Grote Houtstraat 163</h4></div>`
     });
-    
-    if(props.content){
-        var infoWindow = new google.maps.InfoWindow({
-        content:props.content});
 
-    marker.addListener('click', function(){
-        infoWindow.open(map, marker);
-    })
+    addMarker({
+        coords: {
+            lat: 52.37369052720405,
+            lng: 4.877853119049435
+        },
+        content: `<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>Amsterdam</h2><h4>Rozengracht 180</h4></div>`
+    });
 
+    addMarker({
+        coords: {
+            lat: 52.075448555229514,
+            lng: 4.3166986533105876
+        },
+        content: `<div style="text-align: center; font-family: 'Quattrocento', sans-serif; font-weight: 400"><h2>REAL BREAD</br>The Hague</h2><h4>Wagenstraat 182</h4></div>`
+    });
+
+    // Add Marker Function
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        });
+
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            })
+
+        }
     }
-    }
+    //Zoom a location by clicking location button
+    $(".location-button").click(function () {
+        map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
+        map.setZoom(17);
+    });
 }
+
+
+
+
